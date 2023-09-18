c631b826e9e386766664d14781d9e715128fb03b
AvinashKurama <kuramaavinash011@gmail.com>
2023-09-18 20:17:32 +0530

    final-network

--- main/backend.tf
+++ main/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket = "my-terraform-values"
-    key    = "my-terraform-values/terraform-eks-statefile/"
+    key    = "terraform-eks-statefile/tf.statefile"
     region = "us-east-1"
 	role_arn ="arn:aws:iam::676759655484:role/terraform-eks-s3"
   }
